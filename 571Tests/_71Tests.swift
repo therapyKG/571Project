2e02b0933c03c785354d0e5f3b482c3678d11a81
Kurt Gu <kgu10@cs.washington.edu>
2025-05-05 11:13:30 -0700

    Initial Commit

---
+++ 571Tests/_71Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _71Tests.swift
+//  571Tests
+//
+//  Created by Kurt Gu on 5/5/25.
+//
+
+import Testing
+@testable import _71
+
+struct _71Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
